c0b5f41860d0e9b802dca5c75e090119498b008a
Haniif <143860492+Mbulss@users.noreply.github.com>
2024-06-13 09:30:26 +0700

    Delete Final Project directory

--- Final Project/Car-Rental-System/src/ElectricCar.java
+++
@@ -1,9 +0,0 @@
-public class ElectricCar extends Car {
-
-    // Constructor for the ElectricCar class
-    // Takes carId, brand, model, and basePricePerDay as parameters
-    public ElectricCar(String carId, String brand, String model, double basePricePerDay) {
-        // Calls the constructor of the superclass (Car) with the provided parameters
-        super(carId, brand, model, basePricePerDay);
-    }
-}
--- Final Project/Car-Rental-System/src/SUV.java
+++
@@ -1,9 +0,0 @@
-public class SUV extends Car {
-
-    // Constructor for the SUV class
-    // Takes carId, brand, model, and basePricePerDay as parameters
-    public SUV(String carId, String brand, String model, double basePricePerDay) {
-        // Calls the constructor of the superclass (Car) with the provided parameters
-        super(carId, brand, model, basePricePerDay);
-    }
-}
